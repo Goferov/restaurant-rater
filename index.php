39b8e739aa48fa91cde5c32bab003c64b00155be
Goferov <marcingodfryd@gmail.com>
2024-04-09 17:03:52 +0200

    Create simple rendering

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-echo 'Hello world!';
+require_once 'src/controller/AppController.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url( $path, PHP_URL_PATH);
+$action = explode("/", $path)[0];
+$action = $action == null ? 'login': $action;
+
+(new AppController())->render($action);
