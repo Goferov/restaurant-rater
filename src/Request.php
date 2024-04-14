c8c2b9f27533c81950efc554bcba105e53a94fcf
Goferov <marcingodfryd@gmail.com>
2024-04-14 15:03:08 +0200

    Create Request class

---
+++ src/Request.php
@@ -0,0 +1,28 @@
+<?php
+
+class Request {
+    private array $get;
+    private array $post;
+    private array $server;
+    public function __construct(array $get, array $post, array $server) {
+        $this->get = $get;
+        $this->post = $post;
+        $this->server = $server;
+    }
+
+    public function isPost(): bool {
+        return $this->server['REQUEST_METHOD'] === 'POST';
+    }
+
+    public function isGet(): bool {
+        return $this->server['REQUEST_METHOD'] === 'GET';
+    }
+
+    public function get(string $name, $default = null) {
+        return $this->get[$name] ?? $default;
+    }
+
+    public function post(string $name, $default = null) {
+        return $this->post[$name] ?? $default;
+    }
+}
