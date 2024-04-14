8ef3c1729b10448949d4f9b8392a7efa5439836f
Goferov <marcingodfryd@gmail.com>
2024-04-14 19:36:33 +0200

    Create routing class

---
+++ src/Router.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App;
+
+class Router
+{
+    public static $routes;
+
+    public static function get($url, $view)
+    {
+        self::$routes[$url] = $view;
+    }
+
+    public static function post($url, $view)
+    {
+        self::$routes[$url] = $view;
+    }
+
+    static public function run(string $path)
+    {
+        $urlParts = explode("/", $path);
+        $action = $urlParts[0];
+
+        if (!array_key_exists($action, self::$routes))
+            die("Wrong url!");
+
+        $controller = self::$routes[$action];
+        $object = new $controller;
+        $action = $action ?: 'index';
+        $id = $urlParts[1] ?? '';
+
+        $object->$action($id);
+    }
+}
