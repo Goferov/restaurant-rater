d99dcb24a0738abbd3b7cc3e2d77bcd5b90d3921
Goferov <marcingodfryd@gmail.com>
2024-04-15 22:35:51 +0200

    Change views path

--- public/views/template.php
+++ public/views/template.php
@@ -1,5 +1,5 @@
 <?php require_once 'includes/header.php'; ?>
 <main>
-    <?php require_once $page.'.html'; ?>
+    <?php require_once 'pages/'.$page.'.php'; ?>
 </main>
 <?php require_once 'includes/footer.php'; ?>
--- src/controllers/AppController.php
+++ src/controllers/AppController.php
@@ -7,20 +7,21 @@ use App\Request;
 class AppController {
 
     protected Request $request;
+    private static string $main_template_path = 'public/views/template.php';
 
     function __construct()
     {
         $this->request = new Request($_GET, $_POST, $_SERVER);
     }
-    public function render(string $template = null, array $variables = [])
+    public function render(string $page = null, array $variables = []): void
     {
-        $templatePath = 'public/views/'. $template.'.html';
+        $templatePath = 'public/views/pages/'. $page.'.php';
         $output = 'File not found';
 
         if(file_exists($templatePath)) {
             extract($variables);
             ob_start();
-            include $templatePath;
+            include self::$main_template_path;
             $output = ob_get_clean();
         }
         print $output;
