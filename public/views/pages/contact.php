70a97ac5bd37ce1b95a58acce3602bb9642a5da2
Goferov <marcingodfryd@gmail.com>
2024-04-16 20:35:04 +0200

    Add content to contact view

---
+++ public/views/pages/contact.php
@@ -0,0 +1,20 @@
+<section class="container contact ">
+    <div class="mb-mobile-4">
+        <h1 class="section-title f-24">Kontakt</h1>
+        <img src="/public/img/logo.svg" class="mb-2" alt="TopKebab logo"/>
+        <div class="">
+            <a href="tel:+48514343443" class="contact-item me-1 f-24">
+                <i class="fa-solid fa-phone-flip m-mobile-0 f-24" ></i> <span>+48 514 343 443</span>
+            </a>
+            <a href="mailto:redakcja@topkebab.pl" class="contact-item f-24">
+                <i class="fa-solid fa-envelope m-mobile-0 f-24"></i> <span>redakcja@topkebab.pl</span>
+            </a>
+        </div>
+    </div>
+    <div class="text-justify">
+        <h2 class="section-title f-24">O nas</h2>
+        <p class="mb-2">Witaj w TopKebab – Twoim przewodniku po świecie kebabów! Nasza strona powstała z myślą o wszystkich miłośnikach tego pysznego dania. Jesteśmy społecznością, która dzieli się autentycznymi recenzjami i ocenami kebabów z różnych miejsc w Polsce.</p>
+        <p class="mb-2">TopKebab to nie tylko portal, to miejsce spotkań dla smakoszy kebaba. Administratorzy TopKebab to zespół pasjonatów, którzy dbają o to, aby każdy kebab, który znajduje się na naszej stronie, był godny uwagi. To dzięki nim, możesz odkrywać, oceniać i komentować kebaby, które testujesz.</p>
+        <p class="mb-2">Dołącz do nas i pomóż budować największą bazę wiedzy o kebabach w kraju. Twoje opinie pomogą innym znaleźć kebaby, które zasługują na miano najlepszych. Czy jesteś gotowy, aby zostać ekspertem od kebabów? Rozpocznij swoją przygodę z TopKebab już dzisiaj!</p>
+    </div>
+</section>
