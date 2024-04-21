8e8eb6815d11821f33c7d4303e85c0dbbc5fa689
Goferov <marcingodfryd@gmail.com>
2024-04-21 14:09:36 +0200

    Add logout link to panel

--- public/views/pages/panel.php
+++ public/views/pages/panel.php
@@ -4,10 +4,10 @@
     <div class="user-panel">
       <ul class="user-menu">
         <li><a href="#" class="active" title="Zmiana hasła"><i class="fa-solid fa-key"></i> Zmiana hasła</a></li>
-        <li><a href="#" class="f-red" title="Wyloguj się"><i class="fa-solid fa-right-from-bracket"></i> Wyloguj się</a></li>
+        <li><a href="/logout" class="f-red" title="Wyloguj się"><i class="fa-solid fa-right-from-bracket"></i> Wyloguj się</a></li>
       </ul>
       <div class="content">
-          <p class="f-18 mb-2 f-semibold"><?= $message ?></p>
+        <?= $message ? '<p class="f-18 mb-2 f-semibold">'.$message.'</p>' : '' ?>
         <form class="user-form f-medium" action="/changePassword" method="post">
           <input class="input py-1" placeholder="Aktualne hasło" type="password" name="currentPassword" required>
           <input class="input py-1" placeholder="Nowe hasło" type="password" name="newPassword" required>
