fdc1f84709c0c0db0e753a9768a7f8baf00a4729
Goferov <marcingodfryd@gmail.com>
2024-04-21 14:47:05 +0200

    Create addRestaurant view

---
+++ public/views/pages/addRestaurant.php
@@ -0,0 +1,9 @@
+<section class="container">
+    <form action="/" method="post">
+        <input class="input py-1" placeholder="Aktualne hasło" type="password" name="current_password">
+        <input class="input py-1" placeholder="Nowe hasło" type="password" name="new_password" required>
+        <input class="input py-1" placeholder="Powtórz hasło" type="password" name="repeat_new_password" required>
+
+        <button class="button button-primary w-100 f-medium f-18">Zmień hasło</button>
+    </form>
+</section>
