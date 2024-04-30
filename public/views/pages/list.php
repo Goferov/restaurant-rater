c2040fcf6c34f122fa28e1b918ec9c000f1c0789
Goferov <marcingodfryd@gmail.com>
2024-04-30 16:27:29 +0200

    Add border to image in restaurant details, refactor website tag in restaurant list

--- public/views/pages/list.php
+++ public/views/pages/list.php
@@ -33,13 +33,13 @@
                         <h3><?= $restaurant->getName() ?></h3>
                         <div class="tag-list">
                             <div><i class="fa-solid fa-location-dot"></i> <?= $restaurant->getAddress()->getCity() ?></div>
-                            <div><i class="fa-solid fa-globe"></i> <?= $restaurant->getWebsite() ?></div>
+                             <?= $restaurant->getWebsite() ? '<div><i class="fa-solid fa-globe"></i>'.$restaurant->getWebsite().'</div>' : '' ?>
                         </div>
                         <div class="rate d-flex ">
                             <div class="stars">
                                 <?= $reviewHelper->generateStars($restaurant->getRate()) ?>
                             </div>
-                            <div class="f-semibold"><?= number_format($restaurant->getRate(), 1) ?>/5</div>
+                            <div class="f-semibold"><?= number_format($restaurant->getRate(), 1) ?>/5.0</div>
                         </div>
                     </div>
                 </a>
