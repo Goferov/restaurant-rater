8e4f70fb56c43d4305703e8a64f9a4f2b78227e0
Goferov <marcingodfryd@gmail.com>
2024-05-02 11:01:54 +0200

    Add error 404 page

---
+++ public/views/pages/404.php
@@ -0,0 +1,7 @@
+<section class="container text-center">
+    <h1 class="mb-2">404 Not Found</h1>
+    <p class="mb-2">Strona, której szukasz nie istnieje</p>
+    <div class="d-flex justify-content-center">
+        <a href="/" class="button-primary button w-fit-content">Strona główna</a>
+    </div>
+</section>
