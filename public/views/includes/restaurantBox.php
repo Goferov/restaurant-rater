d7737be839541de90e0a9a95f02699644663806b
Goferov <marcingodfryd@gmail.com>
2024-05-03 15:27:41 +0200

    Refactor restaurantBox

--- public/views/includes/restaurantBox.php
+++ public/views/includes/restaurantBox.php
@@ -6,8 +6,8 @@
         <div class="restaurant-details">
             <h3><?= htmlentities($restaurant->getName()) ?></h3>
             <div class="tag-list">
-                <div><i class="fa-solid fa-location-dot"></i> <?= htmlentities($restaurant->getAddress()->getCity()) ?></div>
-                <?= $restaurant->getWebsite() ? '<div><i class="fa-solid fa-globe"></i>'.$restaurant->getWebsite().'</div>' : '' ?>
+                <div><i class="fa-solid fa-location-dot"></i> <span class="location"><?= htmlentities($restaurant->getAddress()->getCity()) ?></span></div>
+                <?= $restaurant->getWebsite() ? '<div><i class="fa-solid fa-globe"></i> <span class="website">'.$restaurant->getWebsite().'</span></div>' : '' ?>
             </div>
             <div class="rate d-flex ">
                 <div class="stars">
