9415d8deb13b2431a8d69e3fe01953b4ab9d8a33
Goferov <marcingodfryd@gmail.com>
2024-05-03 13:35:42 +0200

    Add uploads images, add Config.php

---
+++ src/Config.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App;
+
+class Config {
+    private static $settings = [
+        'db' => [
+            'host' => '',
+            'username' => '',
+            'password' => '',
+            'database' => '',
+            'port' => ''
+        ],
+        'messages' => [
+            'wrongPassword' => 'Hasło jest nieprawidłowe!',
+            'userNotExist' => 'Taki użytkownik nie istnieje!',
+            'userExist' => 'Taki użytkownik już istnieje!',
+            'passwordChange' => 'Hasło zostało zmienione!',
+            'invalidPassword' => 'Hasło musi posiadać liczbę i minimum 6 znaków!',
+            'passwordsNotMatch' => 'Hasła nie są takie same!',
+            'wrongEmail' => 'Email jest nieprawidłowy',
+            'registerComplete' => 'Zostałeś zarejestrowany!',
+            'mustLogin' => 'Musisz się zalogować, aby móc dodawać opinie!',
+            'addedOpinion' => 'Twoja opinia została dodana. Dziękujemy!',
+            'opinionScope' => 'Ocena powinna być w zakresie od 1 do 5',
+            'reviewIsEmpty' => 'Opinia nie może być pusta i musi mieć mniej niż 255 znaków!',
+            'reviewExists' => 'Juz dodałeś opinię do tej restauracji!',
+            'restaurantAdded' => 'Restauracja została dodana!',
+            'requiredFields' => 'Wymagane pola nie mogą być puste.',
+            'wrongUrl' => 'Podany adres strony internetowej jest nieprawidłowy.',
+            'fileTooLarge' => 'Plik jest zbyt duży.',
+            'wrongFileExtension' => 'Nieodpowiedni typ pliku.',
+            'invalidPostalCode' => 'Proszę wprowadzić prawidłowy kod pocztowy (format dd-ddd).',
+            'invalidPhoneNumber' => 'Numer telefonu może zawierać tylko cyfry.',
+        ]
+    ];
+
+    public static function get($key) {
+        $path = explode('.', $key);
+        $value = self::$settings;
+
+        foreach ($path as $piece) {
+            if (isset($value[$piece])) {
+                $value = $value[$piece];
+            } else {
+                return null;
+            }
+        }
+
+        return $value;
+    }
+}
