414d54c56730dc34f5aeb5442ba6315e98cfc88a
Goferov <marcingodfryd@gmail.com>
2024-06-08 16:15:19 +0200

    Init PHPUnit from composer, create RouterTest

---
+++ tests/Unit/RouterTest.php
@@ -0,0 +1,16 @@
+`<?php
+
+use PHPUnit\Framework\TestCase;
+
+class RouterTest extends TestCase
+{
+    public function testPostRoute()
+    {
+
+    }
+
+    public function testGetRoute()
+    {
+
+    }
+}
