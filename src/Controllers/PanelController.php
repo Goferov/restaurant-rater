cb58d0268082982603e6ca1b33dbff500d380cc7
Goferov <marcingodfryd@gmail.com>
2024-06-08 16:14:14 +0200

    Remove session handler from PanelController

--- src/Controllers/PanelController.php
+++ src/Controllers/PanelController.php
@@ -11,14 +11,12 @@ use App\Utils\Session;
 class PanelController extends AppController {
 
     private array $variables;
-    private Session $session;
     private Request $request;
     private IRestaurantRepository $restaurantRepository;
     private Auth $auth;
     private Redirect $redirect;
 
-    public function  __construct(Session $session, Request $request, IRestaurantRepository $restaurantRepository, Auth $auth, Redirect $redirect) {
-        $this->session = $session;
+    public function  __construct(Request $request, IRestaurantRepository $restaurantRepository, Auth $auth, Redirect $redirect) {
         $this->request = $request;
         $this->restaurantRepository = $restaurantRepository;
         $this->auth = $auth;
